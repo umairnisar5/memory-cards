407fa3af691ff324fd1b71bfde28e924b110a96a
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-25 05:44:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+ 
+
+// Keep track of current card
+let currentActiveCard = 0;
+
+
+// Store card data 
+const cardsData = [
+    {
+        question: 'what must a variable begin with?',
+        answer : 'A letter, $ or _'
+    },
+    {
+        question: 'what is a variable?',
+        answer : 'container for a piece of data'
+    },
+    {
+        question : 'Example of case sensitive variable ',
+        answer : 'thisIsAVariable'
+    }
+];
+
+// creat all cards
+function createCards(){
+    cardData.forEach((dat, index) => createCards(data, index));
+ }
+
+ // create a single card in DOM 
+ function
+ 
